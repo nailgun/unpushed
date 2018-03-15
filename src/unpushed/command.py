361590954b370ebd3a07e3bee1b41df54d311caa
Karol Molnár <molnarkarol@gmail.com>
2018-03-15 11:36:01 +0100

    Add option for short messages

--- src/unpushed/command.py
+++ src/unpushed/command.py
@@ -25,6 +25,8 @@ def main():
                       help='manually walk file tree to find repositories')
     parser.add_option('-t', '--tracked', dest='ignore_untracked', action='store_true',
                       help='ignore untracked files in repositories')
+    parser.add_option('-s', '--short', dest='short_info', action='store_true', 
+                      help='print short repository status')
     (options, args) = parser.parse_args()
 
     if not args:
@@ -53,7 +55,10 @@ def main():
     for status in scanner.scan_repos(repos, ignore_untracked=options.ignore_untracked):
         if status['touched'] or options.print_all:
             status_char = '*' if status['touched'] else ' '
-            print(status_char, status['path'], status['status'], '('+status['vcs']+')')
+            if options.short_info:
+                print(status_char, os.path.basename(status['path']), status['status'])
+            else:
+                print(status_char, status['path'], status['status'], '('+status['vcs']+')')
             if options.verbose:
                 print(status['output'])
 
